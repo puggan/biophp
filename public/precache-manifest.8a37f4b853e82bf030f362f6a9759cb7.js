d12e0068b28afc9320aeaffff2e60a6e99ed858d
Thorbjörn Sundragon <puggan@spiro.se>
2019-10-24 16:50:53 +0200

    Copy frontend from https://github.com/GitTuffsRuffs/BioBooking/

---
+++ public/precache-manifest.8a37f4b853e82bf030f362f6a9759cb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7723d6dbc6682ac3a11452674afced1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0edae64374f225e89847",
+    "url": "/static/css/main.2efa56b6.chunk.css"
+  },
+  {
+    "revision": "0b7a9372c2b5fde25268",
+    "url": "/static/js/2.8bf756cc.chunk.js"
+  },
+  {
+    "revision": "0edae64374f225e89847",
+    "url": "/static/js/main.9528b867.chunk.js"
+  },
+  {
+    "revision": "0de7ec616dfd1f72800c",
+    "url": "/static/js/runtime-main.785debff.js"
+  },
+  {
+    "revision": "4e044ba352eb76e560250b208baaeae8",
+    "url": "/static/media/Confetty_Photo_by_Jason_Leung_on_Unsplash.4e044ba3.jpg"
+  },
+  {
+    "revision": "f657f2f92c9688d3f80fbdffba59a286",
+    "url": "/static/media/backgrund_popcorn_Photo_by_twinsfisch_on_Unsplash.f657f2f9.jpg"
+  }
+]);
